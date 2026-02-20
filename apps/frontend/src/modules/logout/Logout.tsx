ff5dc645f182a2dda7bdcd3a816247831fe9fafe
Le-Nam-Pham <phamlenam0102@gmail.com>
2026-02-21 00:17:45 +1100

    Update Logout.tsx

--- apps/frontend/src/modules/logout/Logout.tsx
+++ apps/frontend/src/modules/logout/Logout.tsx
@@ -13,7 +13,7 @@ function Logout() {
     <div className="flex flex-col items-center justify-start">
       <div className="rounded-md py-1 mr-0 shadow-md min-w-screen md:min-w-3/4">
         <h1 className="text-3xl font-bold text-center">You have been logged out</h1>
-        <p className="text-center"> <a href="/login" className="text-main hover:underline">Login</a> to continue the lessons</p>
+        <p className="text-center"> <a href="/login" className="text-main hover:underline font-bold">Login</a> to continue the lessons</p>
       </div>
     </div>
   );
